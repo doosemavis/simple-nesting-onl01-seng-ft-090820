3f20556543f26900c35ad9b1dfec2d488073834c
Moose Davis <moosedavis2011@gmail.com>
2020-09-15 21:35:04 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -107,6 +107,7 @@ def changing_alan
 
 programmer_hash[:alan_kay][:known_for] = "GUI"
 programmer_hash
+
 end
 
 def adding_to_dennis
@@ -125,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+
+programmer_hash[:dennis_ritchie][]
 
 end
